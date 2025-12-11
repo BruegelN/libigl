e41199be59b939163ef52cd4e7e7d574e9120c0e
BruegelN <bruegeln@crashing.systems>
2025-12-11 20:41:17 +0100

    tests: check return value of igl::read_triangle_mesh() and therefore of igl::readOFF() too

--- tests/include/igl/readOFF.cpp
+++ tests/include/igl/readOFF.cpp
@@ -6,7 +6,9 @@ TEST_CASE("readOFF: simple", "[igl]")
     Eigen::MatrixXi F;
     // wait... so this is actually testing test_common::load_mesh not readOFF
     // directly...
-    igl::read_triangle_mesh(test_common::data_path("cube.off"), V, F);
+    bool success = igl::read_triangle_mesh(test_common::data_path("cube.off"), V, F);
+
+    REQUIRE (success == true);
     REQUIRE (V.rows() == 8);
     REQUIRE (V.cols() == 3);
     REQUIRE (F.rows() == 12);
